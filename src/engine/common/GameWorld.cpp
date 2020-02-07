8d9df0acbb5910bec1fa9c2bf563ceafbb98b8b8
Andrew Peterson <andrew_peterson1@brown.edu>
2020-02-07 00:31:10 -0500

    engine architecture

--- src/engine/common/GameWorld.cpp
+++ src/engine/common/GameWorld.cpp
@@ -1,11 +1,26 @@
 #include "GameWorld.h"
 #include "system/DrawSystem.h"
+#include "system/TickSystem.h"
+#include "system/CollisionSystem.h"
 
 GameWorld::GameWorld()
 {
     addSystem<DrawSystem>(std::make_shared<DrawSystem>());
     std::cout << getSystem<DrawSystem>()->getName() << std::endl;
+    addSystem<TickSystem>(std::make_shared<TickSystem>());
+    std::cout << getSystem<DrawSystem>()->getName() << std::endl;
+    addSystem<CollisionSystem>(std::make_shared<CollisionSystem>());
+    std::cout << getSystem<CollisionSystem>()->getName() << std::endl;
+
+    auto it = sys.begin();
+    while (it != sys.end()) {
+        std::cout << it->second->draw() << std::endl;
+        it++;
+    }
     removeSystem<DrawSystem>();
+    removeSystem<TickSystem>();
+    std::cout << getSystem<CollisionSystem>()->getName() << std::endl;
+    removeSystem<CollisionSystem>();
 }
 
 GameWorld::~GameWorld()
